6199a2897370a2ca277164a0ca93764411c1aeec
lucas <lucas@LUCASD-LAPTOP>
2023-09-11 15:23:38 +0200

    Add project files.

---
+++ Uitvoeren.cs
@@ -0,0 +1,13 @@
+﻿namespace Les1poging2
+{
+    internal class Uitvoeren
+    {
+        public static void Main(string[] args)
+        {
+            Opdracht1.Execute();
+            //Opdracht2.Execute();
+            //Opdracht3.Execute();
+            //Opdracht4.Execute();
+        }
+    }
+}
